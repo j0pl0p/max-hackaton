4608c812f04be1561a77a6a2761b7765da0ad19f
error404egor <egoclash129@gmail.com>
2025-11-11 20:34:31 +0300

    project structure created

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,5 +22,10 @@ plugins {
 // If there are changes in only one of the projects, Gradle will rebuild only the one that has changed.
 // Learn more about structuring projects with Gradle - https://docs.gradle.org/8.7/userguide/multi_project_builds.html
 include(":app")
+include(":data")
 
 rootProject.name = "streetImpact"
+include("data")
+include("utils")
+include("bot")
+include("domain")
