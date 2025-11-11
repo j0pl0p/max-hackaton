4e44dbe67b3257804740a085b52810d1fa458acd
error404egor <egoclash129@gmail.com>
2025-11-11 19:54:41 +0300

    some api features test & structure diagram

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -4,6 +4,7 @@ package org.white_powerbank.app
 import okhttp3.internal.wait
 import ru.max.bot.longpolling.LongPollingBot
 import ru.max.bot.annotations.UpdateHandler
+import ru.max.bot.annotations.CommandHandler
 import ru.max.bot.builders.NewMessageBodyBuilder
 import ru.max.botapi.exceptions.ClientException
 import ru.max.botapi.model.ChatAdmin
@@ -15,6 +16,7 @@ import ru.max.botapi.queries.SendMessageQuery
 import java.lang.Thread.sleep
 import kotlin.concurrent.thread
 import ru.max.botapi.model.Message
+import ru.max.botapi.model.TextFormat
 import ru.max.botapi.queries.DeleteMessageQuery
 import ru.max.botapi.queries.EditMessageQuery
 import ru.max.botapi.queries.PostAdminsQuery
@@ -62,7 +64,7 @@ class ReplyBot(val token: String): LongPollingBot(token) {
 
 
         val user_id = update.message?.sender?.userId
-        val chat_admins = ChatAdminsList(listOf(ChatAdmin(user_id, setOf<ChatAdminPermission>(ChatAdminPermission.READ_ALL_MESSAGES))))
+        val chat_admins = ChatAdminsList(listOf(ChatAdmin(user_id, setOf<ChatAdminPermission>(ChatAdminPermission.ADD_ADMINS))))
         val chat_id = update.message?.recipient?.chatId
 
         println("user_id: $user_id\nchat_admins: $chat_admins\nchat_id: $chat_id")
@@ -84,6 +86,12 @@ class ReplyBot(val token: String): LongPollingBot(token) {
 
         SendMessageQuery(client, replyMessage).chatId(update.message?.recipient?.chatId).execute()
 
+
+        val mentionMessage = NewMessageBodyBuilder.ofText("[User mention](https://max.ru/%$user_id%) (ссылка откроется, если у вас установлен MAX)", TextFormat.MARKDOWN).build()
+        val mention_res = SendMessageQuery(client, mentionMessage).chatId(update.message?.recipient?.chatId).execute()
+
+        println("mention res: $mention_res")
+
         println("Sent message: ${text?: "не получил текст:("}")
     }
 }
@@ -91,19 +99,19 @@ class ReplyBot(val token: String): LongPollingBot(token) {
 fun main() {
 
 
-//    println("Starting bot with token $token")
-//    val bot = ReplyBot(token)
-//
-//    try {
-//        bot.start()
-//    } catch (e: Exception) {
-//        println("Error: $e")
-//    }
-//
-//
-//    sleep(100000)
-//
-//    bot.stop()
+    println("Starting bot with token $token")
+    val bot = ReplyBot(token)
+
+    try {
+        bot.start()
+    } catch (e: Exception) {
+        println("Error: $e")
+    }
+
+
+    sleep(100000000)
+
+    bot.stop()
 
 
 
